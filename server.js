a14fe25ff59dc2d5d7c9341c67fb984d2f9202c9
Waqas Abbasi <wa.aliabbasi@gmail.com>
2020-01-17 04:38:23 +0100

    Edited Server.js

--- server.js
+++ server.js
@@ -7,13 +7,6 @@ const cors = require('cors');
 
 app.use(cors());
 
-//     origin: 'https://incomecalculator.netlify.com/'
-// app.use(function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     next();
-// });
-
 app.use(bodyParser.json());
 
 const ip = process.env.IP || '0.0.0.0';
